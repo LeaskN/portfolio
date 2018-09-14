29d1bf6115319e4095a653f63df268591d33f95e
Nicholas Leask <leask.nicholas@gmail.com>
2018-09-14 15:31:10 -0400

    videosync added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Button  } from 'antd';
+import { Icon, Button, Row, Col  } from 'antd';
 
 
 class Projects extends Component {
@@ -15,33 +15,40 @@ class Projects extends Component {
           <a href=" " >CSS </a>
           <a href=" ">JAVASCRIPT</a>
         </div> */}
-        <div className='projectsBox'>
-          <span>
+        <Row className='projectsBox'>
+          <Col span={6}>
             <a href="https://anteup-app.firebaseapp.com/">
               <img className="projectBox1" src="/Media/AnteUp_Front_Page_Image.png" alt=""/>
             </a>
-          </span>
-          <span>
+          </Col>
+          <Col span={6}>
             <a href="https://leaskn.github.io./">
             <img className="projectBox1" src="/Media/Galvanize_Game_Front_Page_Image.png" alt=""/>
             </a>
-          </span>
-          <span>
-            <a href="https://basicnutritionus.firebaseapp.com/NutritionInfo">
+          </Col>
+          <Col span={6}>
+            <a href="https://basicnutritionus.firebaseapp.com">
               <img className="projectBox1" src="/Media/GetFit_Front_Page_Image.png" alt=""/>
             </a>
-          </span>
-          <span>
+          </Col>
+          <Col span={6}>
             <a href="https://timetravel-8e8c7.firebaseapp.com/">
               <img className="projectBox1" src="/Media/TimeToTravel_Front_Page_Image.png" alt=""/>
             </a>
-          </span>
-          <span>
+          </Col>
+          </Row>
+          <Row className='projectsBox'>
+          <Col span={6}>
               <a href="http://university-matcher.surge.sh/">
-            <img className="projectBox1" src="/Media/University_Matcher_Front_Page.png" alt=""/>
+                <img className="projectBox1" src="/Media/University_Matcher_Front_Page.png" alt=""/>
               </a>
-          </span>
-        </div>
+          </Col>
+          <Col span={6}>
+              <a href="https://videosync.io">
+                <img className="projectBox1" src="/Media/video_sync.png" alt=""/>
+              </a>
+          </Col>
+        </Row>
           <Button href="/resume" style={{ color:"#212121" }} className="intro-button" ghost>VIEW RESUME<Icon type="play-circle" style={{ color: '#212121' }} /></Button>
     </div>
     );
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,13 +3,13 @@ import { Icon, Menu, Dropdown } from 'antd';
 const SubMenu = Menu.SubMenu;
 const menu = (
   <Menu>
-    <Menu.Item><a href="http://localhost:3000/">Welcome</a></Menu.Item>
-    <Menu.Item><a href="http://localhost:3000/about">About</a></Menu.Item>
-    <Menu.Item><a href="http://localhost:3000/Projects">Projects</a></Menu.Item>
-    <Menu.Item><a href="http://localhost:3000/Resume">Resume</a></Menu.Item>
+    <Menu.Item><a href="/">Welcome</a></Menu.Item>
+    <Menu.Item><a href="/about">About</a></Menu.Item>
+    <Menu.Item><a href="/Projects">Projects</a></Menu.Item>
+    <Menu.Item><a href="/Resume">Resume</a></Menu.Item>
     <SubMenu title="My Websites">
       <Menu.Item><a href="https://timetravel-8e8c7.firebaseapp.com/">Time Travel</a></Menu.Item>
-      <Menu.Item><a href="https://basicnutritionus.firebaseapp.com/NutritionInfo">Get Fit</a></Menu.Item>
+      <Menu.Item><a href="https://basicnutritionus.firebaseapp.com/">Get Fit</a></Menu.Item>
       <Menu.Item><a href="https://leaskn.github.io./">Galvanize-Zombies</a></Menu.Item>
       <Menu.Item><a href="http://university2-project.surge.sh/">University Matcher</a></Menu.Item>
       <Menu.Item><a href="https://anteup-app.firebaseapp.com/">Ante-Up</a></Menu.Item>
@@ -27,7 +27,7 @@ class Sidebar extends Component {
             </div><br/>
             <div>
               <Dropdown overlay={menu} trigger={['click']}>
-                <a className="ant-dropdown-link" href=".">
+                <a className="ant-dropdown-link" href="">
                 <Icon trigger={['click']} type="menu-unfold"/>
                 </a>
               </Dropdown>
