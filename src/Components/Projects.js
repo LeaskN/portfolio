35a04d41ff1c096714e8fdf09edf29f1b29b2219
Nicholas Leask <leask.nicholas@gmail.com>
2019-01-21 15:57:57 -0500

    scrolling projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Button, Row, Col  } from 'antd';
+import { Icon, Button } from 'antd';
 
 
 class Projects extends Component {
@@ -9,51 +9,89 @@ class Projects extends Component {
         <div className='projects-header'>
           <h1 style={{ margin: '0'}}>Projects:</h1>
         </div>
-        <Row className='projectsBox' style={{ display: 'flex'}}>
-          <Col span={6}>
-              <a href="https://videosync.co">
-                <img className="projectBox1" src="/Media/video_sync.png" alt=""/>
-              </a>
-          </Col>
-          <Col span={6}>
+        <div className="projectsContainer">
+          <div className="projectBox">
+            <a href="https://videosync.co">
+              <img className="projectImage" src="/Media/video_sync.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
             <a href="https://anteup-app.firebaseapp.com/">
-              <img className="projectBox1" src="/Media/AnteUp_Front_Page_Image.png" alt=""/>
+              <img className="projectImage" src="/Media/AnteUp_Front_Page_Image.png" alt=""/>
             </a>
-          </Col>
-          <Col span={6}>
+          </div>
+          <div className="projectBox">
             <a href="https://leaskn.github.io./">
-            <img className="projectBox1" src="/Media/Galvanize_Game_Front_Page_Image.png" alt=""/>
+              <img className="projectImage" src="/Media/Galvanize_Game_Front_Page_Image.png" alt=""/>
             </a>
-          </Col>
-          <Col span={6}>
+          </div>
+          <div className="projectBox">
             <a href="https://basicnutritionus.firebaseapp.com">
-              <img className="projectBox1" src="/Media/GetFit_Front_Page_Image.png" alt=""/>
+              <img className="projectImage" src="/Media/GetFit_Front_Page_Image.png" alt=""/>
             </a>
-          </Col>
-        </Row>
-        <Row className='projectsBox' style={{ display: 'flex' }}>
-          <Col span={6}>
-              <a href="http://university-matcher.surge.sh/">
-                <img className="projectBox1" src="/Media/University_Matcher_Front_Page.png" alt=""/>
-              </a>
-          </Col>
-          <Col span={6}>
-            <a href="https://timetravel-8e8c7.firebaseapp.com/">
-              <img className="projectBox1" src="/Media/TimeToTravel_Front_Page_Image.png" alt=""/>
-            </a>
-          </Col>
-          <Col span={6}>
+          </div>
+          <div className="projectBox">
             <a href="https://dynamicmatrixcssposter.firebaseapp.com/">
-              <img className="projectBox1" src="/Media/DynamicMatrixCSSPoster.png" alt=""/>
+              <img className="projectImage" src="/Media/DynamicMatrixCSSPoster.png" alt=""/>
             </a>
-          </Col>
-          <Col span={6}>
+          </div>
+          <div className="projectBox">
+            <a href="http://university-matcher.surge.sh/">
+              <img className="projectImage" src="/Media/University_Matcher_Front_Page.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://timetravel-8e8c7.firebaseapp.com/">
+              <img className="projectImage" src="/Media/TimeToTravel_Front_Page_Image.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
             <a href="https://starwarsempireposter.firebaseapp.com/">
-              <img className="projectBox1" src="/Media/JoinTheEmpire.png" alt=""/>
+              <img className="projectImage" src="/Media/JoinTheEmpire.png" alt=""/>
             </a>
-          </Col>
-        </Row>
-          <Button href="/resume" style={{ color:"#212121" }} className="intro-button" ghost>VIEW RESUME<Icon type="play-circle" style={{ color: '#212121' }} /></Button>
+          </div>
+          <div className="projectBox">
+            <a href="https://videosync.co">
+              <img className="projectImage" src="/Media/video_sync.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://anteup-app.firebaseapp.com/">
+              <img className="projectImage" src="/Media/AnteUp_Front_Page_Image.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://leaskn.github.io./">
+              <img className="projectImage" src="/Media/Galvanize_Game_Front_Page_Image.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://basicnutritionus.firebaseapp.com">
+              <img className="projectImage" src="/Media/GetFit_Front_Page_Image.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://dynamicmatrixcssposter.firebaseapp.com/">
+              <img className="projectImage" src="/Media/DynamicMatrixCSSPoster.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="http://university-matcher.surge.sh/">
+              <img className="projectImage" src="/Media/University_Matcher_Front_Page.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://timetravel-8e8c7.firebaseapp.com/">
+              <img className="projectImage" src="/Media/TimeToTravel_Front_Page_Image.png" alt=""/>
+            </a>
+          </div>
+          <div className="projectBox">
+            <a href="https://starwarsempireposter.firebaseapp.com/">
+              <img className="projectImage" src="/Media/JoinTheEmpire.png" alt=""/>
+            </a>
+          </div>
+        <Button href="/resume" style={{ color:"#212121" }} className="intro-button" ghost>VIEW RESUME<Icon type="play-circle" style={{ color: '#212121' }} /></Button>
+        </div>
     </div>
     );
   }
