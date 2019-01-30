c08db1c204f6c0f05316b2aa61cd579dbd062597
Nicholas Leask <leask.nicholas@gmail.com>
2019-01-30 15:57:56 -0500

    added neon, too laggy

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -8,12 +8,14 @@ class Intro extends Component {
   render() {
     return (
         <div className="intro" style={{ fontWeight:'bold'}}>
-        <div>
-            Hello, I'm Nicholas Leask,<hr/>
-        </div> 
-        <div>
-            I'm a Fullstack Web Developer!
-        </div>
+          <div className="text" id="red">
+            <div>
+                Hello, I'm Nicholas Leask,<hr/>
+            </div> 
+            <div>
+                I'm a Fullstack Web Developer!
+            </div>
+          </div>
         <Button href="/about" style={{ marginTop: "10%", color:"#212121" }} className="intro-button" ghost>GET TO KNOW ME<Icon type="play-circle" style={{ color: '#212121' }} /></Button>
       </div>
     );
