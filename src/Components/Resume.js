e1e623d1fed25efe4594b95cfd06f971898b91f7
Nicholas Leask <leask.nicholas@gmail.com>
2020-03-06 15:58:03 -0500

    updated resumes

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,10 +5,10 @@ class Resume extends Component {
   render() {
     return (
       <div style={{ zIndex:'1', marginLeft: '5vw'}}>
-        <a download href="/Media/Nic_Leask_Resume_3_5_20.docx" title="NicLeaskResume">
-          <img className="resume" style={{ maxHeight: '80vh', marginTop: '50px', pointerEvents:'all' }} alt="NicLeaskResume" src="/Media/Nic_Leask_Resume.png"/>
+        <a download href="/Media/Nic_Leask_Resume_3_6_20.docx" title="NicLeaskResume">
+          <img className="resume" style={{ maxHeight: '80vh', marginTop: '50px', pointerEvents:'all' }} alt="NicLeaskResume" src="/Media/Nic_Leask_Resume_3_6_20.png"/>
         </a>
-        <div style={{ color:'white', width:'100%', textAlign:'center', pointerEvents:'all'}}>Click to <a className="customLink" download href="/Media/Nic_Leask_Resume_3_5_20.docx" title="NicLeaskResume">download</a></div>
+        <div style={{ color:'white', width:'100%', textAlign:'center', pointerEvents:'all'}}>Click to <a className="customLink" download href="/Media/Nic_Leask_Resume_3_6_20.docx" title="NicLeaskResume">download</a></div>
         <Row style={{ marginBottom:"40px", alignItems: 'center', justifyContent: 'center'}} className="smallOff">
           <Button href="/contact" style={{ marginRight: "10px", color:"#FF3A46", maxWidth: '100px' }} className="intro-button" ghost>Contact</Button>
           <Button href="/" style={{ color:"#212121", maxWidth: '100px' }} className="intro-button" ghost>Home</Button>
